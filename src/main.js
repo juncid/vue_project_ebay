7874f4ee1010ef746a9b702bd75779be95637ae9
LinkaYY <790341284@qq.com>
2018-07-25 21:32:13 +0800

    初始化项目并生成项目结构

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入路由模块
+// import VueRouter from 'vue-router'
+
+// 引入路由
+// import index from './components/index.vue'
+
+// 使用路由中间件
+// Vue.use(VueRouter)
+
+// 定义路由规则
+// const router = new VueRouter({
+//   routes : [
+//    { path: '/index', component: index },
+//  ]
+// })
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  // router
 }).$mount('#app')
