4c0f96c1bbb8d9e824df2689e5ad2e75a3b5c235
LinkaYY <790341284@qq.com>
2018-07-26 15:57:08 +0800

    完成index左边和右边数据渲染

--- src/main.js
+++ src/main.js
@@ -1,25 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
 // 引入路由模块
-// import VueRouter from 'vue-router'
+import VueRouter from 'vue-router'
 
 // 引入路由
-// import index from './components/index.vue'
+import index from './components/index.vue'
 
 // 使用路由中间件
-// Vue.use(VueRouter)
+Vue.use(VueRouter)
 
 // 定义路由规则
-// const router = new VueRouter({
-//   routes : [
-//    { path: '/index', component: index },
-//  ]
-// })
+const router = new VueRouter({
+  routes: [
+    // 访问根目录，去首页
+    { path:'/',redirect:'/index' },
+    {
+      path: '/index',
+      component: index
+    },
+ ]
+})
 
+// 引入CSS
+import './assets/statics/site/css/style.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  // router
+  router
 }).$mount('#app')
